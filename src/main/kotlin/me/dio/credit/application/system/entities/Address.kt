673673609b3dd0fb980fee323d0f0486c1ab6424
DevSauloCouto <saulocouto1221@gmail.com>
2023-12-31 13:14:13 -0300

    jakarta.persistence annotations created in data persistence layer classes

--- src/main/kotlin/me/dio/credit/application/system/entities/Address.kt
+++ src/main/kotlin/me/dio/credit/application/system/entities/Address.kt
@@ -1,6 +1,10 @@
 package me.dio.credit.application.system.entities
 
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
 data class Address(
-    var zipcode: String = "",
-    var street: String = ""
+    @Column(nullable = false) var zipcode: String = "",
+    @Column(nullable = false) var street: String = ""
 )
