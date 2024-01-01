66c4650a6e0c292c26cd822b863da0bed50b859f
DevSauloCouto <saulocouto1221@gmail.com>
2024-01-01 13:42:24 -0300

    field 'income' add in entity Customer

--- src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
@@ -1,6 +1,7 @@
 package me.dio.credit.application.system.entities
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity
 @Table(name = "Customer")
@@ -10,6 +11,8 @@ data class Customer (
         @Column(nullable = false) var lastName: String = "",
 
         @Column(nullable = false, unique = true) val cpf: String,
+
+        @Column(nullable = false) val income: BigDecimal,
 
         @Column(nullable = false, unique = true) var email: String = "",
 
