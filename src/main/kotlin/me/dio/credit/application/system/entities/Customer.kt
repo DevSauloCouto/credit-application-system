390d900ff5a0490aa72b42b74e4f5adf53d2a0e5
DevSauloCouto <saulocouto1221@gmail.com>
2024-01-03 11:08:24 -0300

    fields cpf and income initialized

--- src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
+++ src/main/kotlin/me/dio/credit/application/system/entities/Customer.kt
@@ -10,9 +10,9 @@ data class Customer (
 
         @Column(nullable = false) var lastName: String = "",
 
-        @Column(nullable = false, unique = true) val cpf: String,
+        @Column(nullable = false, unique = true) var cpf: String = "",
 
-        @Column(nullable = false) var income: BigDecimal,
+        @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
 
         @Column(nullable = false, unique = true) var email: String = "",
 
@@ -23,5 +23,5 @@ data class Customer (
         @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST), mappedBy = "customer")
         var credits: List<Credit> = mutableListOf(),
 
-        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null
 )
