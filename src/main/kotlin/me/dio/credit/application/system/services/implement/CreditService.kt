031c0dc365f8de392e66fca7cf3c787635d072da
SauloCouto <saulocouto1221@gmail.com>
2024-01-06 12:32:27 -0300

    logic valid date implemented

--- src/main/kotlin/me/dio/credit/application/system/services/implement/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/services/implement/CreditService.kt
@@ -1,10 +1,12 @@
 package me.dio.credit.application.system.services.implement
 
 import me.dio.credit.application.system.entities.Credit
+import me.dio.credit.application.system.exceptions.DateInvalidException
 import me.dio.credit.application.system.exceptions.NotFoundException
 import me.dio.credit.application.system.repository.CreditRepository
 import me.dio.credit.application.system.services.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -14,6 +16,7 @@ class CreditService(
 ): ICreditService {
 
     override fun save(credit: Credit): Credit {
+        this.validFirstDayInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -31,6 +34,13 @@ class CreditService(
                    credit
                else
                    throw IllegalAccessException("Contact administrator");
+    }
+
+    private fun validFirstDayInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if(dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3)))
+                    true
+                else
+            throw DateInvalidException("It is only possible to request a loan if the payment date of the first installment is 3 months after the requested date")
     }
 
 }
